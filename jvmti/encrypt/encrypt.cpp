8402957eb7a9dfe8af10472354ab854fa6419fbf
wutianxiong <wutianxiong626@gmail.com>
2017-10-10 17:41:43 +0800

    evp-01

--- jvmti/encrypt/encrypt.cpp
+++ jvmti/encrypt/encrypt.cpp
@@ -1,8 +1,80 @@
 #include "jni.h"
 #include <iostream>
 #include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <string.h>
- 
+
+typedef unsigned char uint8;
+
+/**
+ * Create a 256 bit key and IV using the supplied key_data. salt can be added for taste.
+ * Fills in the encryption and decryption ctx objects and returns 0 on success
+ **/
+int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx,
+             EVP_CIPHER_CTX *d_ctx)
+{
+  int i, nrounds = 5;
+  unsigned char key[32], iv[32];
+
+  /*
+   * Gen key & IV for AES 256 CBC mode. A SHA1 digest is used to hash the supplied key material.
+   * nrounds is the number of times the we hash the material. More rounds are more secure but
+   * slower.
+   */
+  i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), salt, key_data, key_data_len, nrounds, key, iv);
+  if (i != 32) {
+    printf("Key size is %d bits - should be 256 bits\n", i);
+    return -1;
+  }
+
+  EVP_CIPHER_CTX_init(e_ctx);
+  EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+  EVP_CIPHER_CTX_init(d_ctx);
+  EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+  return 0;
+}
+
+/*
+ * Encrypt *len bytes of data
+ * All data going in & out is considered binary (unsigned char[])
+ */
+unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
+{
+  /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE -1 bytes */
+  int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
+  unsigned char *ciphertext = malloc(c_len);
+
+  /* allows reusing of 'e' for multiple encryption cycles */
+  EVP_EncryptInit_ex(e, NULL, NULL, NULL, NULL);
+
+  /* update ciphertext, c_len is filled with the length of ciphertext generated,
+    *len is the size of plaintext in bytes */
+  EVP_EncryptUpdate(e, ciphertext, &c_len, plaintext, *len);
+
+  /* update ciphertext with the final remaining bytes */
+  EVP_EncryptFinal_ex(e, ciphertext+c_len, &f_len);
+
+  *len = c_len + f_len;
+  return ciphertext;
+}
+
+/*
+ * Decrypt *len bytes of ciphertext
+ */
+unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len)
+{
+  /* plaintext will always be equal to or lesser than length of ciphertext*/
+  int p_len = *len, f_len = 0;
+  unsigned char *plaintext = malloc(p_len);
+
+  EVP_DecryptInit_ex(e, NULL, NULL, NULL, NULL);
+  EVP_DecryptUpdate(e, plaintext, &p_len, ciphertext, *len);
+  EVP_DecryptFinal_ex(e, plaintext+p_len, &f_len);
+  *len = p_len + f_len;
+  return plaintext;
+}
+
 extern"C" JNIEXPORT jbyteArray JNICALL 
 Java_Encrypt_encrypt(
     JNIEnv * _env, 
@@ -10,66 +82,41 @@ Java_Encrypt_encrypt(
     jbyteArray _buf
 ){
 
-    AES_KEY aes;
-    unsigned char key[AES_BLOCK_SIZE];        // AES_BLOCK_SIZE = 16
-    unsigned char iv[AES_BLOCK_SIZE];        // init vector
-    unsigned char* input_string;
-    unsigned char* encrypt_string;
-    unsigned char* decrypt_string;
-    unsigned int length = 0;// encrypt length (in multiple of AES_BLOCK_SIZE)
-    unsigned int i;
-
     jsize len =_env->GetArrayLength(_buf);
 
-    if ((len + 1) % AES_BLOCK_SIZE == 0) {
-    	length = len + 1;
-    }
-    else {
-    	length = ((len + 1)/ AES_BLOCK_SIZE + 1) * AES_BLOCK_SIZE;
-    }
-    printf("input string len is %d\n",length);
-    // set the input string
-
-    // set the input string
-    input_string = (unsigned char*)calloc(length, sizeof(unsigned char));
-    if (input_string == NULL) {
-    	fprintf(stderr, "Unable to allocate memory for input_string\n");
-    	exit(-1);
-    }
     unsigned char* dst = (unsigned char*)_env->GetByteArrayElements(_buf, 0);
-    memcpy (input_string, dst, len);
- 	// Generate AES 128-bit key
-    for (i = 0; i<16; ++i) {
-    	key[i] = 32 + i;
-    }
-    // Set encryption key
-    for (i = 0; i<AES_BLOCK_SIZE; ++i) {
-    	iv[i] = 0;
-    }
-    if (AES_set_encrypt_key(key, 128, &aes) < 0) {
-    	fprintf(stderr, "Unable to set encryption key in AES\n");
-    	exit(-1);
+
+
+    EVP_CIPHER_CTX en, de;
+
+    unsigned int salt[] = {12345, 54321};
+
+    uint8 *key_data;
+
+    int key_data_len, i;
+
+    uint8 *input;
+
+    key_data = "1243455566";
+    key_data_len = strlen(key_data);
+    /* gen key and iv. init the cipher ctx object */
+    if (aes_init(key_data, key_data_len, (unsigned char *)&salt, &en, &de)) {
+        printf("Couldn't initialize AES cipher\n");
+        return -1;
     }
 
-    // alloc encrypt_string
-    encrypt_string = (unsigned char*)calloc(length, sizeof(unsigned char));
-    if (encrypt_string == NULL) {
-    	fprintf(stderr, "Unable to allocate memory for encrypt_string\n");
-    	exit(-1);
-    }
-    // encrypt (iv will change)
-    AES_cbc_encrypt(input_string, encrypt_string, length, &aes, iv, AES_ENCRYPT);
+    /* encrypt and decrypt each input string and compare with the original */
+    uint8 *plaintext;
+    uint8 *ciphertext;
+    int olen, length;
 
-//    unsigned char* dst = (unsigned char*)_env->GetByteArrayElements(_buf, 0);
-//
-// 	for (int i = 0; i < len; ++i)
-// 	{
-// 		dst[i] = dst[i] ^ 0x07;
-// 	}
+        /* The enc/dec functions deal with binary data and not C strings. strlen() will
+           return length of the string without counting the '\0' string marker. We always
+           pass in the marker byte to the encrypt/decrypt functions so that after decryption
+           we end up with a legal C string */
+    olen = length = len +1;
 
-    //重新分配数组用来存储加密之后的密文，若使用_buf存储会出现数据越界异常
-    jbyteArray array = _env->NewByteArray(length) ;
-    printf("encrypt length is %d \n",length);
-    _env->SetByteArrayRegion(array, 0, length, (jbyte *)encrypt_string);
-    return array;
+    ciphertext = aes_encrypt(&en, dst, &length);
+    _env->SetByteArrayRegion(_buf, 0, length, (jbyte *)ciphertext);
+    return _buf;
 }
